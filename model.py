39e5bf5af381bc3fb769a4e965b4601394449fa9
robrons96 <robin_ronson@yahoo.com>
2017-10-25 22:19:54 -0500

    added utility class as a helper funtion

---
+++ model.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from keras.models import Sequential
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint
+from keras.layers import Lambda, Dropout, Conv2D, MaxPool2D, Dense, Flatten
+from utils import INP
